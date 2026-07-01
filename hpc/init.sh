0cc41c084a020475e04e3263f7f35c5e350813aa
mayhl <michaelangelo.yh.lam@gmail.com>
2026-07-01 10:07:02 -0500

    chore: remove hpc loader/rsync superseded by shared/connect.sh

--- hpc/init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source "${MU_ROOT}/hpc/rsync_cmds.sh"
-source "${MU_ROOT}/hpc/sshfs/init.sh"
-#source ${HPC_CMDS_PATH}/pbs/main.sh
-#source ${HPC_CMDS_PATH}/swap_work_home/main.sh
-#source ${HPC_CMDS_PATH}/
-#source ${HPC_CMDS_PATH}/archive/main.sh
